fa7812a889c59a4b53fb0820f883fb1daa6d16b6
shohan-rhaman <shohanurrahman.contact@gmail.com>
2026-07-23 13:04:55 +0600

    create a new array where every number is increased by 5

--- 02. Map()/script.js
+++ 02. Map()/script.js
@@ -4,18 +4,25 @@ const products = [
     { name: "Watch", price: 250 }
 ];
 
-// I only want the product names.
+const numbers = [5, 10, 15, 20];
 
+
+// I only want the product names.
 const names = products.map(product =>{
     return product.name;
 })
-
 console.log("product name>", names) 
 
-// Show every product with a 10% discount.
 
+// Show every product with a 10% discount.
 const discountPrices = products.map(product =>{
     return product.price * 0.9
 })
-
 console.log("discount prices>", discountPrices)
+
+
+// create a new array where every number is increased by 5
+const increasedNumbers = numbers.map(number =>{
+    return number + 5;
+})
+console.log("increased Number>", increasedNumbers);
