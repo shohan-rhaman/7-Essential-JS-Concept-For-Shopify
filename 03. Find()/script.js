15839605ec1d81c6e41a353bb75dd2c0736155fb
shohan-rhaman <shohanurrahman.contact@gmail.com>
2026-07-24 11:05:21 +0600

    shopify example

--- 03. Find()/script.js
+++ 03. Find()/script.js
@@ -12,3 +12,17 @@ const filterResult = numbers.filter(number =>{
     return number > 10;
 })
 console.log("Filter Result", filterResult) // [ 15, 20, 25 ] we will get array
+
+
+// ================ Shopify Example ============================= \\
+const products = [
+    { id: 1, name: "Nike", price: 120 },
+    { id: 2, name: "Adidas", price: 90 },
+    { id: 3, name: "Watch", price: 250 }
+];
+
+// Suppose customer clicks on product ID 2. we need that product.
+const product = products.find(product =>{
+    return product.id === 2;
+})
+console.log("Specific Product", product)
