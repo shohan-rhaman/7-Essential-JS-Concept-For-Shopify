54d0c778a8066ca6a2b27c09e92940f8ee8ecdc0
shohan-rhaman <shohanurrahman.contact@gmail.com>
2026-07-26 14:35:19 +0600

    shopify example

--- 05. some()/script.js
+++ 05. some()/script.js
@@ -5,3 +5,17 @@ const result = numbers.some(num =>{
 })
 console.log("Result>", result) // return True
 
+
+// ============= Shopify Example ======================
+
+const products = [
+    { name: "Nike", stock: 5 },
+    { name: "Adidas", stock: 0 },
+    { name: "Watch", stock: 10 }
+];
+// Q: Is there at least one product that is out of stock?
+
+const hasOutOfStock = products.some(product =>{
+    return product.stock === 0
+})
+console.log("Out of stock", hasOutOfStock) // Return: true
