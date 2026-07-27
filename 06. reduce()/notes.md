62166d1780c343707acf9e3f30b41e1d7eb56121
shohan-rhaman <shohanurrahman.contact@gmail.com>
2026-07-27 09:05:02 +0600

    Shopify Example 1

--- 06. reduce()/notes.md
+++ 06. reduce()/notes.md
@@ -3,3 +3,11 @@
 const result = array.reduce((accumulator, currentValue) => {
     return updatedAccumulator;
 }, initialValue);
+
+
+const numbers = [10, 20, 30];
+const total = numbers.reduce((sum, number)=>{
+    return sum + number;
+}, 0)
+
+sum = 0;
--- 06. reduce()/script.js
+++ 06. reduce()/script.js
@@ -4,3 +4,16 @@ const total = numbers.reduce((sum, number)=>{
     return sum + number;
 }, 0)
 console.log("Total Numbers", total) // Return: 60
+
+
+// ============= Shopify Example 1 ===================
+const cart = [
+    { name: "Nike", quantity: 2 },
+    { name: "Watch", quantity: 1 },
+    { name: "Cap", quantity: 5 }
+];
+// Q: Calculate the total cart quantity.
+const totalQuantity = cart.reduce((totalCart, product) =>{
+    return totalCart + product.quantity
+}, 0)
+console.log("Total Cart", totalQuantity) // Return: 8
